e8466e280c6c9465a01a099f373ef3c2ce5e9e72
Adriano Lima de Souza <adrianolima.dev@gmail.com>
2026-05-26 19:10:21 -0300

    Adiciona novos projetos

--- config/projects.php
+++ config/projects.php
@@ -2,12 +2,12 @@
 
 return [
     [
-        'title' => 'Título do Projeto',
-        'excerpt' => 'Descrição curta do projeto para cards menores.',
-        'description' => 'Descrição do projeto com mais detalhes para os cards de destaque.',
-        'image' => '/images/projects/projeto.webp',
-        'demo_url' => 'https://projeto.meusite.com',
-        'github_url' => 'https://github.com/user/projeto-php',
+        'title' => 'Portfólio pessoal',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
         'packagist_url' => '',
         'tags' => [
             'PHP',
@@ -17,4 +17,132 @@ return [
         'year' => 2026,
         'status' => 'completed'
     ],
+    [
+        'title' => 'Espaço Ideal',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
+        'packagist_url' => '',
+        'tags' => [
+            'PHP',
+            'Composer',
+            'PSR-4'
+        ],
+        'year' => 2026,
+        'status' => 'completed'
+    ],
+    [
+        'title' => 'E-commerce API',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
+        'packagist_url' => '',
+        'tags' => [
+            'PHP',
+            'Composer',
+            'PSR-4'
+        ],
+        'year' => 2026,
+        'status' => 'completed'
+    ],
+    [
+        'title' => 'Catálogo E-commerce',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
+        'packagist_url' => '',
+        'tags' => [
+            'PHP',
+            'Composer',
+            'PSR-4'
+        ],
+        'year' => 2026,
+        'status' => 'completed'
+    ],
+    [
+        'title' => 'CMS headless',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
+        'packagist_url' => '',
+        'tags' => [
+            'PHP',
+            'Composer',
+            'PSR-4'
+        ],
+        'year' => 2026,
+        'status' => 'completed'
+    ],
+    [
+        'title' => 'Note Framework',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
+        'packagist_url' => '',
+        'tags' => [
+            'PHP',
+            'Composer',
+            'PSR-4'
+        ],
+        'year' => 2026,
+        'status' => 'completed'
+    ],
+    [
+        'title' => 'WMS',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
+        'packagist_url' => '',
+        'tags' => [
+            'PHP',
+            'Composer',
+            'PSR-4'
+        ],
+        'year' => 2026,
+        'status' => 'completed'
+    ],
+    [
+        'title' => 'App de ações',
+        'excerpt' => 'Descrição curta do projeto.',
+        'description' => 'Descrição do projeto com mais detalhes.',
+        'image' => '/assets/images/unsplash.jpg',
+        'demo_url' => '',
+        'github_url' => 'https://github.com/user/projeto',
+        'packagist_url' => '',
+        'tags' => [
+            'PHP',
+            'Composer',
+            'PSR-4'
+        ],
+        'year' => 2026,
+        'status' => 'completed'
+    ],
+    // [
+    //     'title' => 'Título do Projeto',
+    //     'excerpt' => 'Descrição curta do projeto.',
+    //     'description' => 'Descrição do projeto com mais detalhes.',
+    //     'image' => '/assets/images/unsplash.jpg',
+    //     'demo_url' => '',
+    //     'github_url' => 'https://github.com/user/projeto',
+    //     'packagist_url' => '',
+    //     'tags' => [
+    //         'PHP',
+    //         'Composer',
+    //         'PSR-4'
+    //     ],
+    //     'year' => 2026,
+    //     'status' => 'completed'
+    // ],
 ];
